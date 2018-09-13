1ece8993c57b73fd94949187938de26cef0c19b7
Anishgoofle <anishojha82@gmail.com>
2018-09-14 01:13:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation.js';
+import Signin from './components/Signin/Signin.js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import './App.css';
+
+
+const app = new Clarifai.App({
+ apiKey: 'a070ae18896c4d9d95d914fd5bb91386'
+});
+
+const particlesOptions ={
+  particles: {
+      number:{
+        value: 50,
+         density:{
+          enable: true,
+          value_area: 800
+         }
+      }
+    }
+  }
+
+
+class App extends Component { 
+  constructor() {
+    super();
+    this.state = {
+       input: '',
+       imageUrl: '',
+       box: {},
+       route: 'signin'
+    }
+  }
+
+
+
+calculateFaceLocation = (data) => {
+ 
+ const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+ const image = document.getElementById('inputimage');
+ const width  = Number(image.width);
+ const height = Number(image.height);
+ return {
+  leftCol: clarifaiFace.left_col * width,
+  topRow: clarifaiFace.top_row * height,
+  rightCol: width - (clarifaiFace.right_col * width),
+  bottomRow: height - (clarifaiFace.bottom_row * height)
+
+ }
+}
+
+displayFaceBox = (box) =>
+{
+  this.setState({box: box});
+}
+
+
+onInputChange = (event) => {
+   this.setState({input: event.target.value});
+ }
+
+onButtonSubmit = () => {
+  this.setState({imageUrl: this.state.input});
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, 
+  this.state.input)
+  .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+  .catch(err => console.log(err));
+  
+}
+
+
+onRouteChange = (route) => {
+ this.setState({route: route});
+
+}
+
+  render() {
+    return (
+      <div className="App">
+         <Particles className ='particles'
+            params = {particlesOptions}
+          />
+          <Navigation onRouteChange={this.onRouteChange} />
+          {this.state.route === 'signin'
+           ? <Signin onRouteChange = {this.onRouteChange} />
+           : <div>
+             <Logo />
+             <Rank />
+             <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit = {this.onButtonSubmit} />
+             <FaceRecognition box={this.state.box} imageUrl= {this.state.imageUrl}/>
+             </div>
+           }
+      </div>
+    );
+  }
+}
+
+export default App;
