48716a036a330a336e86c3f621fabdd6f5cee54b
Anish Ojha <anishojha82@gmail.com>
2019-10-11 12:20:12 +0530

    setup FE on heroku

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -33,7 +33,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn() {
-    fetch("http://localhost:3000/register", {
+    fetch("https://salty-depths-27736.herokuapp.com/register", {
       method: "post",
       headers: {
         "Content-Type": "application/json"
